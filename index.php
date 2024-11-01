2c5a0f9acddea57052c76d826025f4b7666b6831
Rpf1105 <rogeriofilho1105@gmail.com>
2024-11-01 13:22:47 -0300

    Adição do css e modificação no index

--- index.php
+++ index.php
@@ -2,8 +2,26 @@
 <html>
     <head>
         <title>Nome da pagina</title>
+        <link rel="stylesheet" href="recursos/estilo.css">
     </head>
     <body>
-    
+        <div class="content">
+            <div class="nav">
+                <ul class>
+                    <li><a href="/index.php">Página inicial</a></li>
+                    <li><a href="">Sobre nós</a></li>
+                </ul>
+            </div>
+            <div class="main">
+                <H1>coloca o formulario</H1>
+                <form action="">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email"><br>
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senha"><br>
+                    <input type="submit" value="enviar">
+                </form>
+            </div>
+        </div>
     </body>
 </html>
