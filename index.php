789e359317b13f9e53de582473c19b06e98f3ac9
Rpf1105 <rogeriofilho1105@gmail.com>
2024-11-03 16:14:37 -0300

    restruturacao da navegaçao

--- index.php
+++ index.php
@@ -5,15 +5,24 @@
         <link rel="stylesheet" href="recursos/estilo.css">
     </head>
     <body>
-        <div class="content">
+        <header>
+            <h1><a href="">SITE</a></h1>
             <div class="nav">
                 <ul class>
                     <li><a href="/index.php">Página inicial</a></li>
                     <li><a href="">Sobre nós</a></li>
                 </ul>
             </div>
+            <div class="login">
+                <ul class>
+                    <li><a href="">Cadastrar</a></li>
+                    <li><a href="">Entrar</a></li>
+                </ul>
+            </div>
+        </header>
+        <div class="content">
             <div class="main">
-                <H1>coloca o formulario</H1>
+                <H1>Pagina de login</H1>
                 <form action="">
                     <label for="email">Email</label>
                     <input type="email" name="email" id="email"><br>
